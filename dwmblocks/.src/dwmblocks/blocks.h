54c88ad20b8e6811c3ce63ac6de0940523b9fb44
Jonesad-ETSU <jonesad@etsu.edu>
2020-11-06 00:21:10 -0500

    11.06.20

---
+++ dwmblocks/.src/dwmblocks/blocks.h
@@ -0,0 +1,12 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+  {"",      "/home/jonesad/.scripts/draw-vol-bar.sh $(( $(pamixer --get-volume) / 10 )) 8 $(pamixer --get-mute > /dev/null 2>&1 && echo 'X' || echo '#')",          10,         11},
+	{" ", "date '+%a, %I:%M%p'",					5,		0},
+  {"\0",    "/home/jonesad/.scripts/battery.sh", 10, 11},
+  {";","",0,0}, //MOVES FOLLOWING TO BOTTOM BAR
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = "  ";
+static unsigned int delimLen = 2;
